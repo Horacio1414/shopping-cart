e283e01a4f229e77e9ada7360f6c0fec37dadeb7
Horacio Demaria Hontan <horacio.demaria@globant.com>
2023-04-08 17:22:03 -0300

    create ui for shopping cart
    consume data from a mock
    
    use react context

---
+++ src/components/Filters.jsx
@@ -0,0 +1,50 @@
+import { useState, useId } from "react"
+import "./Filters.css"
+import { useFilters } from "./hooks/useFilters.jsx"
+
+export function Filters () {
+
+    const {filters, setFilters} = useFilters()
+
+    const [minPrice, setMinPrice] = useState(0)
+    const minPriceFilterId = useId()
+    const categotyFilterId = useId()
+
+    const handleChangeMinPrice = (event) => {
+        
+        setFilters(prevState =>( {
+            ...prevState,
+            minPrice: event.target.value
+        }))
+    }
+
+    const handleChangeCategory = (event)=> {
+        setFilters(prevState=> ({
+            ...prevState,
+            category:event.target.value
+        }))
+    }
+
+    return (
+        <section className="filters">
+            <div>
+                <label htmlFor={minPriceFilterId}>Price</label>
+                <input type="range" id={minPriceFilterId}
+                min="0"
+                max="1000"
+                onChange={handleChangeMinPrice}
+                value={filters.minPrice}
+                />
+            </div>
+            <span>${filters.minPrice}</span>
+            <div>
+                <label htmlFor={categotyFilterId}>Category</label>
+                <select id={categotyFilterId} onChange={handleChangeCategory}>
+                    <option value="all">All</option>
+                    <option value="laptops">Laptops</option>
+                    <option value="smartphones">smartphones</option>
+                </select>
+            </div>
+        </section>
+    )
+}
